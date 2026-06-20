ebde0611abfd6031fc7c2ef9153c74bb01fee7f7
kamaalismailahmed-cmyk <email-kaaga-github@tusaale.com>
2026-06-20 11:52:27 +0300

    Fix environment variables for production

--- school_backend/src/config/db.js
+++ school_backend/src/config/db.js
@@ -2,20 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, // Halkan ayaad ku beddeshay
+  ssl: {
+    rejectUnauthorized: false // Neon DB waxay u baahan tahay tan
+  }
 });
 
 pool.on('connect', () => {
   console.log('✅ PostgreSQL connected successfully!');
-});
-
-pool.on('error', (err) => {
-  console.error('❌ Unexpected error on idle client', err);
-  process.exit(-1);
 });
 
 module.exports = pool;
