5d98a3e318a9f8391d232c67c0d1ac5be62edcd8
kamaalismailahmed-cmyk <email-kaaga-github@tusaale.com>
2026-06-20 16:03:39 +0300

    Fix environment variables for production

--- school_backend/src/routes/paymentRoutes.js
+++ school_backend/src/routes/paymentRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/paymentController');
 
+// 1. Endpoint-ka lagu darayo lacag bixinta cusub
 router.post('/add', controller.createPayment);
+
+// 2. Endpoint-ka lagu soo helayo taariikhda lacag bixinta ee arday gaar ah
 router.get('/history/:studentId', controller.getHistory);
 
-// KU DAR KHADKAN SI UU 404-KA U BAXO:
+// 3. Endpoint-ka lagu helayo wadarta dakhliga (Dashboard)
 router.get('/total-income', controller.getTotalIncome);
 
 module.exports = router;
